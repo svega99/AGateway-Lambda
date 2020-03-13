1c9704a02e7732276e20814ad7d89be7c0012b87
Santiago Vega <santiago.vega-r@labinfo.is.escuelaing.edu.co>
2020-03-13 12:10:36 -0500

    FIRST

---
+++ src/main/java/edu/escuelaing/services/MathServices.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package edu.escuelaing.services;
+
+/**
+ *
+ * @author santiago.vega-r
+ */
+public class MathServices {
+     public static Integer square(Integer i){
+        return i*i;
+    }
+}
